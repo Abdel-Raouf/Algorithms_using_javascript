2cc5bf8ce2b871369edcf384ad425e19a7dddf74
abdelraouf <rofarewsh@gmail.com>
2016-11-10 20:27:47 +0200

    adding pseudo code for insertion sort

--- Sort_Algorithms/Insertion_Sort.js
+++ Sort_Algorithms/Insertion_Sort.js
@@ -17,3 +17,13 @@ var solutionArray = [5, 8, 9, 10, 14, 20];
 var sortedArray = insertion_Sort(initialArray);
 var passed = JSON.stringify(sortedArray)==JSON.stringify(solutionArray);
 console.log('Passed:' + passed);
+
+
+// for j = 2 to length[A]
+// 	do key = A[j]
+// 	>insert A[j] into the sorted sequence A[1 ... j-1]
+// 	i = j - 1
+// 	while i > 0  and A[i] > key
+// 		do A[i+1] = A[i]
+// 			i = i - 1
+// 	A[i+1] = key
